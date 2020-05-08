0cb72d43d6bc27035d2e071c3f1438774624e223
Alexandra Nicolau <alexandra.tnicolau@gmail.com>
2020-05-08 23:23:21 +0100

    Added input field for playing again

--- diceRoll.py
+++ diceRoll.py
@@ -1,11 +1,20 @@
 import random
+import time
 min = 1
 max = 12
 
-roll_again = "yes"
+roll_again = True
 
-while roll_again == "yes" or roll_again == "y":
-    print("Wait for it")
-    print("Results are in")
-    print(random.randint(min, max))
-    print(random.randint(min, max))
+# while roll_again == "yes" or roll_again == "y":
+#     print("Wait for it")
+#     print("Results are in!")
+#     print(random.randint(min, max))
+#     print(random.randint(min, max))
+
+while roll_again:
+    print("Wait for it...")
+    time.sleep(random.randint(1, 2))
+    result = print(random.randint(min, max))
+    # print(result)
+    roll_again = False
+    txt = input("Let's have another go?")
