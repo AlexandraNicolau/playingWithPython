119f163282e482aa046835a3f9ec2a359d081a9f
Alexandra Nicolau <alexandra.tnicolau@gmail.com>
2020-05-08 23:12:53 +0100

    added small random function for rolling dice

--- diceRoll.py
+++ diceRoll.py
@@ -5,6 +5,7 @@ max = 12
 roll_again = "yes"
 
 while roll_again == "yes" or roll_again == "y":
-    print "Wait for it"
-    print "Results are in"
-    print random.randInt(min, max)
+    print("Wait for it")
+    print("Results are in")
+    print(random.randint(min, max))
+    print(random.randint(min, max))
